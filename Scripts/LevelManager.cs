2580f304d379ee8e143d4644d43835a225b65bf9
Hoang Phuc <hvphuc081299@gmail.com>
2018-11-18 21:36:31 +0700

    Move scenses

---
+++ Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel() {
+		Application.LoadLevel ("GamePlay");
+	}
+	public void QuitGame() {
+		Application.Quit ();
+	}
+	public void LoadMenuGame() {
+		Application.LoadLevel ("Start Menu");
+	}
+	public void LoadEndGame() {
+		Application.LoadLevel ("EndGame");
+	}
+}
