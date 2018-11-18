03e834211d0cdd30435b6d7dd79b835b002dff71
Hoang Phuc <hvphuc081299@gmail.com>
2018-11-18 21:36:03 +0700

    Controll activate of game

---
+++ Scripts/GamePlay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+using UnityEngine.UI;
+
+
+public class GamePlay : MonoBehaviour {
+	int max, min;
+	int guess;
+	public Text guessText;
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+
+	void StartGame() {
+		max = 1000;
+		min = 0;
+		NextGuess ();
+		max = max + 1;
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess ();
+	}
+
+	void NextGuess() {
+		guess = Random.Range (min, max);
+		guessText.text = guess.ToString ();
+	}
+}
